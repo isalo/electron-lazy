fc2de6c2be43f63b4e81716c73546806db92455d
Ivan Salo <isalo@eagleinvsys.com>
2020-06-06 15:59:15 +0300

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import SimpleComponent from './SimpleComponent';
+//const SimpleComponent = React.lazy(() => import('./SimpleComponent'));
+
+const loading = (
+	<div className="pt-3 text-center">
+	  <div className="sk-spinner sk-spinner-pulse"></div>
+	</div>
+  )  
+
+const App = () => {
+	return (
+		<div className='app'>
+			<React.Suspense fallback={loading}>
+				<SimpleComponent/>
+			</React.Suspense>
+		</div>
+	)
+}
+
+export default App
